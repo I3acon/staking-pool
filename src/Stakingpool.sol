9530605e58b20838e91b68f2a4bfd8c495221067
I3acon <poonawitch@gmail.com>
2023-05-02 18:06:30 +0700

    fix: factory

--- src/Stakingpool.sol
+++ src/Stakingpool.sol
@@ -1,13 +1,15 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity 0.8.15;
+pragma solidity ^0.8.15;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 import "./interfaces/IETHlaunchpad.sol";
+import "forge-std/console.sol";
 import "./Xedon.sol";
 
-contract Stakingpool is Ownable {
+contract Stakingpool is AccessControl {
     mapping(address => bool) private stakers;
     mapping(address => uint256) public balances;
+    bool public isUpload = false;
     bool public isDeposit = false;
     uint256 pool_balances;
 
@@ -23,29 +25,32 @@ contract Stakingpool is Ownable {
 
     constructor(
         address _launchpad,
-        address _token,
-        bytes memory _pubkey,
-        bytes memory _withdrawal_credentials,
-        bytes memory _signature,
-        bytes32 _deposit_data_root
+        address _token
     ) {
+        console.log(_launchpad);
+        console.log(_token);
         require(_launchpad != address(0), "Invalid launchpad address");
         require(_token != address(0), "Invalid token address");
-        // require(
-        //     keccak256(_toWithdrawalCred(address(this))) ==
-        //         keccak256(_withdrawal_credentials),
-        //     "Invalid withdrawal credentials"
-        // );
-
+         _setupRole(DEFAULT_ADMIN_ROLE, tx.origin);
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
         // todo check pubkey , signature , deposit_data_root
 
         launchpad = IETHlaunchpad(_launchpad);
         token = Xedon(_token);
+    }
+
+    function uploadDepositData( bytes memory _pubkey,
+        bytes memory _withdrawal_credentials,
+        bytes memory _signature,
+        bytes32 _deposit_data_root)public onlyRole(DEFAULT_ADMIN_ROLE)
+        {
+        require(keccak256(_toWithdrawalCred(address(this))) == keccak256(_withdrawal_credentials),"Invalid withdrawal credentials");
         pubkey = _pubkey;
         withdrawal_credentials = _withdrawal_credentials;
         signature = _signature;
         deposit_data_root = _deposit_data_root;
-    }
+        isUpload = true;
+        }
 
     function stake() public payable {
         require(msg.value == 8 ether, "Amount must be equal to 8 ETH");
@@ -54,6 +59,7 @@ contract Stakingpool is Ownable {
             "Contract balance cannot exceed 32 ETH"
         );
         require(!stakers[msg.sender], "Already stake");
+        require(isUpload);
 
         balances[msg.sender] += msg.value;
         stakers[msg.sender] = true;
@@ -62,7 +68,6 @@ contract Stakingpool is Ownable {
         if (address(this).balance == 32 ether) {
             depositToLaunchpad();
         }
-
         emit Deposit(msg.sender, msg.value);
     }
 
@@ -78,7 +83,7 @@ contract Stakingpool is Ownable {
     }
 
     function withdraw() public {
-        require(stakers[msg.sender], "Not stakers account");
+        require(token.balanceOf(msg.sender) == 1);
         require(isDeposit, "Not deposit yet");
         address staker1 = token.ownerOf(1);
         address staker2 = token.ownerOf(2);
@@ -112,7 +117,7 @@ contract Stakingpool is Ownable {
     }
 
     // This function is used for testing purpose only
-    function rugpool() public onlyOwner {
+    function rugpool() public {
         payable(msg.sender).transfer(address(this).balance);
     }
 }
--- test/Factory.t.sol
+++ test/Factory.t.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.15;
 
 import "forge-std/Test.sol";
 import "../src/Factory.sol";
+import "../src/Stakingpool.sol";
 
 contract FactoryTest is Test {
     Factory public factory;
@@ -11,7 +12,7 @@ contract FactoryTest is Test {
 
 
     function setUp() public {
-        factory = new Factory();
+        factory = new Factory(0xff50ed3d0ec03aC01D4C79aAd74928BFF48a7b2b);
         goerliFork = vm.createFork(GOERLI_URL);
     }
 
@@ -21,17 +22,12 @@ contract FactoryTest is Test {
         assertEq(vm.activeFork(), goerliFork); 
     }
 
-    function testCreateToken() public {
-        vm.startPrank(address(0x01))
-        bytes32 salt = factory.stringToBytes32(
-            "hello this is sound from dekwat"
-        );
-        address launchpad = 0xff50ed3d0ec03aC01D4C79aAd74928BFF48a7b2b;
-        factory.createToken(salt, launchpad);
-        address token = factory.tokens(1);
-        bytes memory bytecode = factory.getBytecode(launchpad, token);
-        address predicted = factory.getAddress(bytecode, salt);
-        assertEq(predicted, token);
+    function testCreatePool() public {
+        factory.createPool();
+    }
+
+    function  testUploadData() public {
+
     }
 
     // function testBalance()public {
@@ -56,30 +52,5 @@ contract FactoryTest is Test {
     //     factory.toWithdrawalCred(predicted);
     // }
 
-    function testCreatePool() public {
-        bytes32 salt = factory.stringToBytes32(
-            "hello this is sound from dekwat"
-        );
-        address launchpad = 0xff50ed3d0ec03aC01D4C79aAd74928BFF48a7b2b;
-        factory.createToken(salt, launchpad);
-        address token = 0x104fBc016F4bb334D775a19E8A6510109AC63E00;
-        factory.getAddress(factory.getBytecode(launchpad, token), salt);
-        bytes
-            memory pubkey = "0x84bf1094cec37dc1f990956718f4bdcaa4d85d11398e18e870b13993b21f19df75f10fa0b769c9a704b3bd5bc4b87e06";
-        bytes
-            memory signature = "0xae440bd6d8fe806410c62a882d853ab2afc15a05c7f0372138b7f276a69028089e65fe2be018127090f883e1c3754447098b7dcf014c71fee5855567efa8ac89ef8129f5535f78a6af36e60d5ef74bea19f7c3774502997a241202b51f0da947";
-        bytes32 deposit_data_root = 0x492d7d9ce701beffd5da73657afd7b2aa8eaad4a6dd88e3d4a0271deba792f34;
-        bytes
-            memory crend = "0x01000000000000000000000094e525b7c97d5f394c56c633562aa5ee7a2c5894";
-        //  0x01000000000000000000000094e525b7c97d5f394c56c633562aa5ee7a2c5894
-        factory.createPool(
-            launchpad,
-            token,
-            salt,
-            pubkey,
-            crend,
-            signature,
-            deposit_data_root
-        );
-    }
+  
 }
