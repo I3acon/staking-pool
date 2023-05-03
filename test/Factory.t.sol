a28efb0c65f61edb299673cad3b3e72fc49b83a6
poonawitch <59003699+poonawitch@users.noreply.github.com>
2023-05-03 16:32:48 +0700

    feat: test-deposit to contract and launchpad

--- test/Factory.t.sol
+++ test/Factory.t.sol
@@ -12,14 +12,22 @@ contract FactoryTest is Test {
     Xedon public token;
     uint256 goerliFork;
     string GOERLI_URL = vm.envString("GOERLI_URL");
+    address payable public alice =
+        payable(0x01a56263e8c5B3F51a8a2fD37faa463523C1d604);
+    address payable public bob =
+        payable(0x02a6CBf724be0824c8f6E764D34F495Ccf62A98d);
+    address payable public carol =
+        payable(0x03ab15f59fb568Ce901cB1e0f6C11e7DDd53B535);
+    address payable public david =
+        payable(0x04ab67759693c0b7cCBE4fF219777c911a4b9541);
 
     function setUp() public {
         factory = new Factory(0xff50ed3d0ec03aC01D4C79aAd74928BFF48a7b2b);
         // goerliFork = vm.createFork(GOERLI_URL);
+        // vm.selectFork(goerliFork);
     }
 
     // function testFork() public {
-    //     vm.selectFork(goerliFork);
     //     emit log_uint(block.chainid);
     //     assertEq(vm.activeFork(), goerliFork);
     // }
@@ -29,37 +37,53 @@ contract FactoryTest is Test {
     }
 
     function testUploadData() public {
+        vm.record();
+        goerliFork = vm.createFork(GOERLI_URL);
+        vm.selectFork(goerliFork);
+        startHoax(alice);
         token = new Xedon();
         stakingpool = new Stakingpool(
             payable(0xff50ed3d0ec03aC01D4C79aAd74928BFF48a7b2b),
             address(token)
         );
+        token.giveAccess(address(stakingpool));
         bytes
             memory pubkey = hex"84bf1094cec37dc1f990956718f4bdcaa4d85d11398e18e870b13993b21f19df75f10fa0b769c9a704b3bd5bc4b87e06";
         bytes
-            memory withdrawal_credent = hex"010000000000000000000000f62849f9a0b5bf2913b396098f7c7019b51a820a";
+            memory withdrawal_credent = hex"010000000000000000000000afcca70fde030f521ed7ab99bfa80749ceeeb8be";
         bytes
-            memory sig = hex"a0e6950e9373b1a7b30ca923ba0e1ca06afaaca64650a34d891f75c9cc76995c3b88efdb22a74be4c9b59d2918993ac30f114aebd0e360505eda81348611ed5ab0b4edea11b5334f27c4c1475bcf7a6d9daa5a16c27d8ade863781456d95bd42";
-        bytes32 deposit_data_root = 0xd56eaadba77aaee1bcc9edc08e204a1b6777bb85523bcc257f8c208e1386e5d8;
+            memory sig = hex"8a1587f908664ee1360951525cfbe196ca699d17e2a211307119e556595b8bc0a31329727a15b61eeb49dbdaa8edce4607cd3e74890773fd1c2e3c0d4283d048b3fb9610f692806a3a00cad424116fb894f085eb65a481fe645def2fb8e4e413";
+        bytes32 deposit_data_root = 0x39811f3ff5ee67efe8d91312c08bef0933196a11528a19e74b521086ca143107;
         stakingpool.uploadDepositData(
             pubkey,
             withdrawal_credent,
             sig,
             deposit_data_root
         );
+        stakingpool.stake{value: 8 ether}();
+        assertEq(address(stakingpool).balance, 8 ether);
+        assertEq(token.ownerOf(1), alice);
+        vm.stopPrank();
 
-        // stakingpool._toWithdrawalCred(address(stakingpool));
+        hoax(bob);
+        stakingpool.stake{value: 8 ether}();
+        assertEq(address(stakingpool).balance, 16 ether);
+        assertEq(token.ownerOf(2), bob);
 
-        // console.log(keccak256(stakingpool._toWithdrawalCred(address(stakingpool))));
-        // console.log(string(keccak256(withdrawal_credent)));
-        // console.log(
-        //     keccak256(stakingpool._toWithdrawalCred(address(stakingpool))) ==
-        //         keccak256(withdrawal_credent)
-        // );
+        hoax(carol);
+        stakingpool.stake{value: 8 ether}();
+        assertEq(address(stakingpool).balance, 24 ether);
+        assertEq(token.ownerOf(3), carol);
+
+        hoax(david);
+        stakingpool.stake{value: 8 ether}();
+        assertEq(token.ownerOf(4), david);
+        assertEq(address(stakingpool).balance, 0);
     }
 
-    // function testBalance()public {
-    //     vm.Prank(alice);
+    // function testStake() public {
+    //     // startHoax(alice);
+    //     stakingpool.stake{value: 8 ether}();
     // }
 
     // function testStrToBytes(string memory _str) public view returns (bytes32) {
